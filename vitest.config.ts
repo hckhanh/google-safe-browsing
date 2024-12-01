fd5f63fb2258091ead052c6eea9b19e65cce9869
Khánh Hoàng <hi@khanh.id>
2024-12-01 15:16:32 +0700

    chore(testing): Add coverage step to GitHub Actions CI workflow (#27)
    Introduced a new `vitest.config.ts` file to manage testing configuration, enabling broader coverage reporting and modifying reporters based on CI environment. Updated `.github/workflows/tests.yml` to include a coverage job which runs unit tests with coverage enabled and uploads the report to Codacy. Additionally, updated `pnpm-lock.yaml` to reflect new dependencies related to coverage tools.

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import {
+  configDefaults,
+  coverageConfigDefaults,
+  defineConfig,
+} from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      enabled: false,
+      provider: 'v8',
+      exclude: [
+        '*.config.ts',
+        '*.config.js',
+        ...coverageConfigDefaults.exclude,
+      ],
+      reporter: process.env.CI ? ['clover'] : coverageConfigDefaults.reporter,
+    },
+    reporters: process.env.CI
+      ? ['dot', 'github-actions']
+      : configDefaults.reporters,
+  },
+})
